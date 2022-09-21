0726905f88b9be6eebe7f77e26b2a76494497b80
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-09-21 21:40:28 +0300

    update

--- lesson_two.py
+++ lesson_two.py
@@ -1,8 +1,13 @@
 # primitive types 
+
 a = 5.0 ## float
 b = 4  ## int
 c = "String" ## str
 d = True     ## bool
+
+
+
+
 
 ## collections
 x = [] # list
@@ -71,12 +76,40 @@ if not(condition or condition2 and not(condition)):
 long_last_name = "BenimovichLiel"
 this_is_a_list = ["may", "gil", "maor", "tom", "elad",5, True, 6.1]
 
+print(this_is_a_list[0]) # -> may
+print(this_is_a_list[1]) # -> gil
+print(this_is_a_list[2]) # -> maor
+print(this_is_a_list[-1]) # -> 6.1
+print(this_is_a_list[-4]) #
+print(this_is_a_list[4]) #
+
+
 # print(type(this_is_a_list))
 
+# for i in long_last_name:
+#     print(i)
 
 # for index in range(len(long_last_name)):
 #     print(long_last_name[index])
 
-print("Ilay", "Yair", sep="^_^", end="+$")
+# print("Ilay", "Yair", sep="^_^", end="+$")
+"""
+ 1. print every element with "Good Morning" in front of it ["may", "gil", "maor", "tom", "elad"]
+#example: "Good Morning may"
+          "Good Morning gil"
+          "Good Morning maor"
+          "Good Morning tom"
+          "Good Morning elad"
+ 2. print every element with "Good Night" in front of it in reverse order
+          "Good Night elad"
+          "Good Night tom"
+          "Good Night maor"
+          "Good Night gil"
+          "Good Night may"
+3. print intertwine of the names in the list BONUS
+example: "Good Morning may"
+         "Good Night elad"
+         "Good Morning gil"
+         "Good Night tom"
 
-
+"""
