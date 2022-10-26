a2eda8e8573354f5d2ff2c8ff60db26d01778ed7
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-10-26 19:53:10 +0300

    some password generator algos

--- lesson_five.py
+++ lesson_five.py
@@ -4,6 +4,9 @@
 multi line comment
 """
 
+import time
+from random import randint
+
 counter = 1
 
 for i in "Hello World":
@@ -16,7 +19,7 @@ x = counter != len("Hello World")
 
 data_base = {
     "user" : "KingElad@hotmail.com",
-    "password" : "375",
+    "password" : "37524018",
 }
 
 possible_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+"
@@ -31,7 +34,27 @@ user_name = "KingElad@hotmail.com"
 user_pass = ""
 user_try = ""
 
-# # brote force attack
+
+# start = time.time()
+
+# for i in range(100000):
+#     print(time.time() - start)
+
+
+# end = time.time()
+
+# last_result = 2.563169479370117
+
+
+# print(end - start) 
+ 
+
+# if (last_result > (end - start)):
+#     print("you are faster")
+
+
+# #brote force attack
+# start = time.time()
 # for i in range(10): # 10
 #     for j in range(10): # 100
 #         for k in range(10): # 1000
@@ -42,12 +65,40 @@ user_try = ""
 #                             for c in range(10): # 100000000
 #                                 user_try = str(i) + str(j) + str(k) + str(w) + str(q) + str(e) + str(d) + str(c)
 #                                 if user_try == data_base["password"]:
+#                                     end = time.time()
 #                                     user_pass = user_try
 #                                     print("password is: " + user_pass)
-#                                     break
+#                                     if user_name == data_base["user"] and user_pass == data_base["password"]:
+#                                         print("Welcome to the system")
+#                                         print("time: " + str(end - start))
+#                                     else:
+#                                         print(user_pass)                                   
 
 
-# if user_name == data_base["user"] and user_pass == data_base["password"]:
-#     print("Welcome to the system")
-# else:
-#     print(user_pass)
+possible_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+"
+old_adv_point = "Emily_04041984"
+adv_point = "Emily_04041984$"
+
+random_og_password = ""
+# for i in range(2):
+#     random_og_password += possible_chars[(randint(0, len(possible_chars)-1))]
+
+print(random_og_password)
+
+for i in range(1000000):
+    random_password = old_adv_point
+    start = time.time()
+
+    for j in range(1):
+        random_password += possible_chars[(randint(0, len(possible_chars)-1))]
+
+    end = time.time()
+
+    if random_password == adv_point:
+        print("success! your password is :" , adv_point)
+        print("password is: " + random_password)
+        print("number of tries: " + str(i))
+        exit()
+
+    print(random_password)
+    # print("time: " + str(end - start))
