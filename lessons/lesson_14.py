b7d378b8166a81596241c4bd9746262cb8e2c1df
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-11 19:50:49 +0200

    update 11/01/23

--- lessons/lesson_14.py
+++ lessons/lesson_14.py
@@ -1,28 +1,63 @@
-
-max = 0
-min = 0
-command = ""
-zero_flag = True
-while command.lower() != "quit":
+## ex1
+# max = 0
+# min = 0
+# command = ""
+# zero_flag = True
+# while command.lower() != "quit":
     
-    user_input = input("Please Enter a number or quit to exit the program: ")
+#     user_input = input("Please Enter a number or quit to exit the program: ")
 
-    if user_input.lower() == "quit":
-        break
-    try:
-        user_input = int(user_input)
-    except:
-        print("invalid input")
+#     if user_input.lower() == "quit":
+#         break
+#     try:
+#         user_input = int(user_input)
+#     except:
+#         print("invalid input")
 
-    if user_input > max:
-        max = user_input
+#     if user_input > max:
+#         max = user_input
 
-    if user_input < min or (zero_flag == True):
-        min = user_input
-        zero_flag = False
+#     if user_input < min or (zero_flag == True):
+#         min = user_input
+#         zero_flag = False
 
     
+# print("the maximun:" , max)
+# print("the minimum:", min)
 
 
-print("the maximun:" , max)
-print("the minimum:", min)
+
+class BankAccunt():
+
+    def __init__(self, first_deposit):
+        self.balance = first_deposit
+
+    def deposit(self, amount):
+        if(amount > 0):
+            self.balance += amount
+
+    def withrow(self, amount):
+        ## 
+        pass
+
+
+
+
+bank_account = BankAccunt(1000)
+bank_account.deposit(-100)
+
+print(bank_account.shakshoka)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
