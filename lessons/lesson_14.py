2bcb5977a2b25c1f8d4eea7d79e130f4378d6572
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-11 18:36:25 +0200

    update 11/01/23

---
+++ lessons/lesson_14.py
@@ -0,0 +1,15 @@
+
+max = 0
+min = 0
+command = ""
+
+while command.lower() != "quit":
+    
+    user_input = input("Please Enter a number or quit to exit the program")
+
+    #enter code here//
+    pass
+
+
+print("the maximun:" , max)
+print("the minimum:", min)
