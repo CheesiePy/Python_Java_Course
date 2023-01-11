7c7c93049b0f36d1052fb1097117c64041d5eec4
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-11 19:28:19 +0200

    update 11/01/23

--- lessons/lesson_14.py
+++ lessons/lesson_14.py
@@ -2,13 +2,26 @@
 max = 0
 min = 0
 command = ""
-
+zero_flag = True
 while command.lower() != "quit":
     
-    user_input = input("Please Enter a number or quit to exit the program")
+    user_input = input("Please Enter a number or quit to exit the program: ")
 
-    #enter code here//
-    pass
+    if user_input.lower() == "quit":
+        break
+    try:
+        user_input = int(user_input)
+    except:
+        print("invalid input")
+
+    if user_input > max:
+        max = user_input
+
+    if user_input < min or (zero_flag == True):
+        min = user_input
+        zero_flag = False
+
+    
 
 
 print("the maximun:" , max)
