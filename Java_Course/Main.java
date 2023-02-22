8bbeff0077f050f845f2d93cc57f47709d162000
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-02-22 19:25:45 +0200

    update 22/02/23

---
+++ Java_Course/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        Car mycar = new Car(1996, 100);
+        Car yourcar = new Car(2016, 150);
+        System.out.println(yourcar.year);
+        System.out.println(mycar.year);
+    }
+}
