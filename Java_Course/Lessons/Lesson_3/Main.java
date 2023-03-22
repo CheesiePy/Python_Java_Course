03be88facd62411522d98a3ade6662a9981364fc
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-03-22 23:48:55 +0200

    update 22/03/23

--- Java_Course/Lessons/Lesson_3/Main.java
+++ Java_Course/Lessons/Lesson_3/Main.java
@@ -4,15 +4,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String user_input = "";
-        int token = 0;
-        while (!user_input.equals("quit")){
-            System.out.print("Please Enter a number: ");
-            user_input = sc.nextLine();
-            System.out.println(user_input);    
-        }
-        
+        // Scanner sc = new Scanner(System.in);
+        // String user_input = "";
+        // int token = 0;
+        // while (!user_input.equals("quit")){
+        //     System.out.print("Please Enter a number: ");
+        //     user_input = sc.nextLine();
+        //     System.out.println(user_input);    
+        // }
+        Task t = new Task();
+        t.task1(5, "JavaIsTheBest!");
+        System.out.println(t.task2(1994));
+        int sum =  t.task4(50000000);
+        int sumbad = t.task4_bad(50000000);
+        System.out.println("good algo: " + sum);
+        System.out.println("normal algo: " + sumbad);
+
+        String rev = t.task5("רק פושטק עלוב בלוע קטשופ קר");
+        System.out.println(rev);
+
     }
     
 }
--- Java_Course/Lessons/Lesson_3/Task.java
+++ Java_Course/Lessons/Lesson_3/Task.java
@@ -2,41 +2,68 @@ package Lessons.Lesson_3;
 class Task{
 
     public void task1(int n, String name){
-        // print name n times 
+        for(int i = 0 ; i < n; i++){
+            System.out.println(name);
+        }
     }
 
     public int task2(int n){
-        // return the first even number after n
-        return 0;
+        if (n%2 == 0){
+            return n + 2;
+        }
+        return n + 1;
     }
 
     public boolean task3(int n){
         // return true if n is even, or flase otherwise
-        return true;
+        if(n%2 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 
     // Method 4
     public int task4(int n){
         // calculate and return the sum of all numbers from 1 to n
-        return 0;
+        return (1 + n)*n/2;
+    }
+
+    public int task4_bad(int n){
+        // calculate and return the sum of all numbers from 1 to n
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+        return sum;
     }
 
     // Method 5
     public String task5(String str){
         // reverse the order of characters in a string and return it
-        return null;
+        String reverce = "";
+        for(int i = 0; i < str.length(); i++){
+            reverce += str.charAt(str.length() - 1 - i);
+        }
+        return reverce;
     }
 
     // Method 6
     public int task6(int[] arr){
         // find and return the largest integer in an array
+
         return 0;
     }
 
     // Method 7
     public boolean task7(String str){
         // check if a string is a palindrome and return true or false
+        // רק פושטק עלוב בלוע קטשופ קר V
+        // אמא אבא V
+        // dad mom V
+        // רגע = > עגר X
         return false;
     }
 
