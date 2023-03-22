95cd85ce760054e152ca18549cf30e03a0a4fe52
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-03-22 19:23:14 +0200

    update 22/03/23

---
+++ Java_Course/Lessons/Lesson_3/Main.java
@@ -0,0 +1,18 @@
+package Lessons.Lesson_3;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String user_input = "";
+        int token = 0;
+        while (!user_input.equals("quit")){
+            System.out.print("Please Enter a number: ");
+            user_input = sc.nextLine();
+            System.out.println(user_input);    
+        }
+        
+    }
+    
+}
